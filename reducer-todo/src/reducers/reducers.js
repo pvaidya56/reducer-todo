269419044eb26127f1a10e95bb7161b2f7cc92c7
Priya <pvaidya56@gmail.com>
2019-11-21 17:34:29 -0800

    created todo app

---
+++ reducer-todo/src/reducers/reducers.js
@@ -0,0 +1,24 @@
+export const initialState = {
+    tasks: [],
+    item: 'Reducers',
+    completed: false,
+    id: 3892987589
+  }
+  
+  export const reducer = (state, action) => {
+    switch(action.type) {
+      case 'ADD_TASK':
+        const newTask = {
+          item: action.payload,
+          completed: false,
+          id: Date.now()
+        }
+        return {
+          ...state,
+          tasks: [...state.tasks, newTask]
+        };
+  
+      case 'MARK_COMPLETED':
+      default: return state
+    }
+  }
